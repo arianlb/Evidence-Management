b660707b669e09f48bad21b9f4f91ac43d0a54cf
arianlb <83365444+arianlb@users.noreply.github.com>
2022-05-07 09:27:50 -0400

    agregando las clases controladoras y las rutas

---
+++ routes/planRouter.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validate } = require('../middlewares/validateFields');
+const { existsRole } = require('../helpers/dbValidators');
+
+const router = Router();
+
+
+module.exports = router;
