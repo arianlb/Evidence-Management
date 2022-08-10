2c25fd1f4b9eb8cdf78df0cbbde73dce5467e0aa
arianlb <83365444+arianlb@users.noreply.github.com>
2022-08-10 18:29:32 -0400

    add ToDo a la respuesta de criterio

--- src/helpers/indicatorResponse.js
+++ src/helpers/indicatorResponse.js
@@ -2,53 +2,53 @@ const Indicator = require('../models/indicator');
 const User = require('../models/user');
 
 const indicatorByCriterion = async (area) => {
-    const indicators = await Indicator.find( {model: true} );
+    const indicators = await Indicator.find({ model: true });
     let objectives = [];
-    for(objective of area.objectives){
+    for (objective of area.objectives) {
 
         let criterions = [];
-        for(criterion of objective.criterions){
+        for (criterion of objective.criterions) {
 
             let indicator;
-            for(indi of indicators){
-                
-                if(indi.criterion){
-                    if(indi.criterion.equals(criterion._id)){
-                        indicator = {_id: indi._id, name: indi.name};
+            for (indi of indicators) {
+
+                if (indi.criterion) {
+                    if (indi.criterion.equals(criterion._id)) {
+                        indicator = { _id: indi._id, name: indi.name };
                         break;
                     }
 
                 }
             }
-            criterions.push({_id: criterion._id, name: criterion.name, status: criterion.status, indicator});
+            criterions.push({ _id: criterion._id, name: criterion.name, status: criterion.status, todo: criterion.todo, indicator });
         }
 
-        objectives.push({_id: objective._id, name: objective.name, criterions});
+        objectives.push({ _id: objective._id, name: objective.name, criterions });
     }
 
     return objectives;
 }
 
-const indicatorsByCategory = async(categories = ['TRABAJO DOCENTE-EDUCATIVO EN PREGRADO Y POSGRADO',
-'TRABAJO POLÍTICO-IDEOLÓGICO', 'TRABAJO METODOLÓGICO', 'TRABAJO DE INVESTIGACIÓN E INNOVACIÓN',
-'SUPERACIÓN'], userId) => {
+const indicatorsByCategory = async (categories = ['TRABAJO DOCENTE-EDUCATIVO EN PREGRADO Y POSGRADO',
+    'TRABAJO POLÍTICO-IDEOLÓGICO', 'TRABAJO METODOLÓGICO', 'TRABAJO DE INVESTIGACIÓN E INNOVACIÓN',
+    'SUPERACIÓN'], userId) => {
 
     let indicators;
     let indicatorsResponse = [];
 
-    if(userId) {
+    if (userId) {
         const user = await User.findById(userId).populate('indicators');
 
-        if(user.indicators.length > 0) {
-            
-            for(let i = 0; i < categories.length; i++) {
+        if (user.indicators.length > 0) {
+
+            for (let i = 0; i < categories.length; i++) {
                 indicators = [];
-                indicatorsResponse.push({category: categories[i], indicators});
+                indicatorsResponse.push({ category: categories[i], indicators });
             }
-    
+
             user.indicators.forEach(element => {
-                for(let i = 0; i < categories.length; i++) {
-                    if(element.category === categories[i]){
+                for (let i = 0; i < categories.length; i++) {
+                    if (element.category === categories[i]) {
                         indicatorsResponse[i].indicators.push(element);
                         break;
                     }
@@ -58,10 +58,10 @@ const indicatorsByCategory = async(categories = ['TRABAJO DOCENTE-EDUCATIVO EN P
 
         return indicatorsResponse;
     }
-    
-    for(let i = 0; i < categories.length; i++) {
-        indicators = await Indicator.find({category: categories[i], model: true});
-        indicatorsResponse.push({category: categories[i], indicators});
+
+    for (let i = 0; i < categories.length; i++) {
+        indicators = await Indicator.find({ category: categories[i], model: true });
+        indicatorsResponse.push({ category: categories[i], indicators });
     }
 
     //indicators = await Indicator.find().sort({field: 'asc', category: 'desc'});
