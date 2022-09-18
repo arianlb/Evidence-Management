a5d9bc5c4df8d5d92be84529c8a31ae9c617eed5
arianlb <83365444+arianlb@users.noreply.github.com>
2022-09-18 13:01:16 -0400

    los tokens no expiran

--- src/helpers/generateJWT.js
+++ src/helpers/generateJWT.js
@@ -4,9 +4,7 @@ const jwt = (uid = '', role = '', name = '', username = '', department = '') => 
 
     return new Promise( (resolve, reject) => {
         const payload = { uid, role, name, username, department };
-        jsonwebtoken.sign(payload, process.env.SECRETORPRIVATEKEY, {
-            expiresIn: '4h'
-        }, (err, token) => {
+        jsonwebtoken.sign(payload, process.env.SECRETORPRIVATEKEY, (err, token) => {
             if(err){
                 console.log(err);
                 reject('No se pudo generar el token');
